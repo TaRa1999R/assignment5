c28a6ea9f095155c75117ae4e2c9699169e1fe95
TaRa1999R <rezaietara78@gmail.com>
2023-07-25 01:35:07 +0330

    assignment5

--- 3.py
+++ 3.py
@@ -1,14 +1,27 @@
 print (" ♦♦ PRINTING A DIMOND ♦♦ ")
 print (" Please enter the size od the dimond as an input ")
 
-def line ( max, i ) :
+def line ( khat , max ) :
+    stars = 2 * khat + 1
+    empty = max - stars 
+
+    for i in range ( int ( empty / 2 )) :
+        print (" ", end = "")
+        
+    for i in range ( stars ) :
+        print ("*", end = "")
+        
+    for i in range ( int ( empty / 2 )) :
+        print (" ", end = "")
+    
+    print ("")
 
 size = int ( input (" Please enter the size of the dimond : "))
 
-most_star = 2 * size - 1 
+most_stars = 2 * size - 1 
 
-for i in range ( 1 , most_star + 1 , 2 ) :
-   line () 
+for i in range ( size ) :
+    line ( i , most_stars  )
 
-#for i in range ( most_star - 2 , 0 , -2 ) :
-    
+for i in range ( size - 2 , -1 , -1 ) :
+     line ( i , most_stars)
