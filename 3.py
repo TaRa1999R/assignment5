4e8350567bd38358244241e344ff973a0b9f5db4
TaRa1999R <rezaietara78@gmail.com>
2023-07-25 00:33:51 +0330

    assignment5

--- 3.py
+++ 3.py
@@ -1,14 +1,14 @@
-# print (" ♦♦ PRINTING A DIMOND ♦♦ ")
-# print (" Please enter the size od the dimond as an input ")
+print (" ♦♦ PRINTING A DIMOND ♦♦ ")
+print (" Please enter the size od the dimond as an input ")
 
-# def line ( max, i ) :
+def line ( max, i ) :
 
-# size = int ( input (" Please enter the size of the dimond : "))
+size = int ( input (" Please enter the size of the dimond : "))
 
-# most_star = 2 * size - 1 
+most_star = 2 * size - 1 
 
-# for i in range ( 1 , most_star + 1 , 2 ) :
-#    line () 
+for i in range ( 1 , most_star + 1 , 2 ) :
+   line () 
 
-# #for i in range ( most_star - 2 , 0 , -2 ) :
+#for i in range ( most_star - 2 , 0 , -2 ) :
     
